21d399510732c9ff00a32c25d14374c13bb092d5
OMEGAKPT <guid.gamers.wp@gmail.com>
2019-05-22 19:23:04 +0200

    22/05/2019 Update

--- Oneiric/Oneiric/Sprites/Character.cs
+++ Oneiric/Oneiric/Sprites/Character.cs
@@ -1,6 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 abstract class Character : Sprite
 {
+    public int Level { get; set; }
+    public int MaxiumLife { get; set; }
+    public int ActualLife { get; set; }
+    public int LifeIncreaser { get; set; }
+    public int MaxiumPm { get; set; }
+    public int ActualPm { get; set; }
+    public int PmIncreaser { get; set; }
+    public int Damage { get; set; }
+    public int DamageIncreaser { get; set; }
+    public int Defense { get; set; }
+    public int DefenseIncreaser { get; set; }
+    public int Speed { get; set; }
+    public int SpeedIncreaser { get; set; }
+    public List<Skill> Skills { get; set; }
 
+    public Character() { }
 }
